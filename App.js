08ced13d3aaa4bbcb8cc15b8dde144da401415b0
IidaPeltonen <n0peii00@students.oamk.fi>
2022-11-04 11:47:28 +0200

    apin rajapinnan testausta

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const URL = 'https://api.fingrid.fi/';
+const APIKEY = 'D1fcMx4D0O6ua4wwnEGHZ9re1lFBsREE5G2tNZ8D';
 
 export default function App() {
+  const [data, setData] = useState('')
+  useEffect(() => {
+    axios
+      .get(URL, {headers:{'x-api-key':APIKEY}})
+      .then(response => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      .catch(error => {
+        alert(error)
+        console.log('ei onnaa')
+      })
+  }, [])
+     
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <div id="center">
+jee
+    </div>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,3 +36,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
