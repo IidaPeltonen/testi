56e225a2a2cea396b1719df97beb9e6ade6bc323
IidaPeltonen <n0peii00@students.oamk.fi>
2022-11-07 14:43:05 +0200

    toimii

--- App.js
+++ App.js
@@ -1,30 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 
-const URL = 'https://api.fingrid.fi/';
+//testi, hakee tuntihinnastosta hinnan 1.11 10-11
+const URL = 'https://api.fingrid.fi/v1/variable/317/events/json?start_time=2022-11-07T13%3A00%3A00Z&end_time=2022-11-07T13%3A00%3A00Z';
 const APIKEY = 'D1fcMx4D0O6ua4wwnEGHZ9re1lFBsREE5G2tNZ8D';
+
+//https://api.fingrid.fi/v1/variable/ tämä alku on aina sama
+//%09317/events/json? tämä pätkä muuttuu, tässä tapauksessa meidän käyttämä "taulukko" on 317, eli variableId on 318
+//start_time=2022-11-01T10%3A00%3A00Z&end_time=2022-11-01T11%3A00%3A00Z'; tässä on sitten se aikaväli, miltä hinta haetaan
+
+
+//jos haluaa ajan NYT, tai tietyllä hetkellä, sekä alku, että loppuaika pitää olla samat, muoto on 
+//siis 2022-11-07T13:00:00Z
 
 export default function App() {
   const [data, setData] = useState('')
+
   useEffect(() => {
-    axios
-      .get(URL, {headers:{'x-api-key':APIKEY}})
-      .then(response => {
-        setData(response.data)
-        console.log(response.data)
-      })
-      .catch(error => {
-        alert(error)
-        console.log('ei onnaa')
-      })
-  }, [])
+    fetch (URL, {
+      headers: {
+        'method': 'GET',
+        'x-api-key': APIKEY
+      }
+    })
+
+    .then(response => response.json())
+    .then((json) => {
+      console.log(json)
+    }, (error) => {
+      console.log(error)
+    })
+  })
      
   return (
-    <div id="center">
-jee
-    </div>
+
+      <Text>jjiksrjnjt</Text>
+
   )
 }
 
@@ -36,4 +48,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
